b56f73b20f70c6cc5df7ec3576cdf2d517b65a47
Michael37 <smcculm952@ops.org>
2015-12-10 10:53:54 -0600

    Review on Random Numbers

---
+++ Lesson 9/Exercises/Randome (Review).py
@@ -0,0 +1,14 @@
+# Michael McCullough
+# Period 4
+# 12/10/15
+
+# Review: Making a six sided die
+
+import random
+
+num1 = random.randint(1,6)
+num2 = random.randint(1,6)
+
+num3 = num1 + num2
+
+print ("You rolled %d" %(num3))
